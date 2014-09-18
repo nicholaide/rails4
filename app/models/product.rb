bd57499c4b78ffe71f02e442c661f10a31f24f2d
Nicholai de Guzman <nicholaideguzman@nis-imac.dhcp.swmed.org>
2014-09-18 13:11:30 -0500

    done with task D

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
+
+  #associations
+  has_many :line_items
+  
+  #callsbacks
+  before_destroy :ensure_not_referenced_by_any_line_item
   
   #validations  
   validates :title, :description, :image_url, presence: true
@@ -10,5 +16,16 @@ message: 'must be a URL for GIF, JPG or PNG image.'}
   def self.latest
     Product.order(:updated_at).last
   end
-
+  
+  private
+  
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
+      
 end
