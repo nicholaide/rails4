72d446416776b359bbcda8cab10b82d2c404f23a
Nicholai de Guzman <nicholaideguzman@nis-imac.dhcp.swmed.org>
2014-09-25 17:29:22 -0500

    finish iteration G1

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,11 @@ class Order < ActiveRecord::Base
   #associations
   has_many :line_items, dependent: :destroy
   
-end
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
+  
+end #end of class
