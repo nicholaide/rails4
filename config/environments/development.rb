00ee5bc1d08dd15c7a0694ac915a0175d6070f0f
Nicholai de Guzman <Nicholai@cchinfatt.local>
2014-10-07 21:37:04 -0500

    finished with iteration H1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  #don't send during development
+  config.action_mailer.delivery_method = :test
+
+  
 end
