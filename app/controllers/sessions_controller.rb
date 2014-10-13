7cfb137b350c846c1fa8b42961cc7817d0538fbf
Nicholai de Guzman <nicholaideguzman@nis-imac.dhcp.swmed.org>
2014-10-13 10:49:21 -0500

    end of I2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_(name: params[:name])
+    user = User.find_by(name: params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
